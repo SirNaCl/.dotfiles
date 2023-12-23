67f886dd2e14adca365947a2645f5d25c9a23578
Emil Gedenryd <emil.gedenryd@gmail.com>
2023-12-23 14:11:11 +0100

    Remove auto save

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -33,6 +33,7 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
+          "python",
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
